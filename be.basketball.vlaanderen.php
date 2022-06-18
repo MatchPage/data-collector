43363a428afe7c7fab2a612d3396bf1cd03c0b2b
MatseVH <matse.vanhorebeek@gmail.com>
2022-06-18 14:16:10 +0200

    first commit

---
+++ be.basketball.vlaanderen.php
@@ -0,0 +1,30 @@
+<?php
+// Get all the matches from the api
+function matches($issguid)
+{
+  // VBL api url to get matches
+  $api_url = "https://vblcb.wisseq.eu/VBLCB_WebService/data/OrgMatchesByGuid?issguid=" . $issguid;
+  // Get the data
+  $data = file_get_contents($api_url);
+  // Decode the data
+  $data = json_decode($data, true);
+  // Return the data
+  return $data;
+}
+
+// Sort the matches by date
+function sortByDate($matches)
+{
+  // Sort array by jsDTCode (Unix timestamp)
+  $matchesSort = array();
+  foreach ($matches as $match) {
+    foreach ($match as $key => $value) {
+      if (!isset($matchesSort[$key])) {
+        $matchesSort[$key] = array();
+      }
+      $matchesSort[$key][] = $value;
+    }
+  }
+}
+
+// 
